535fef1ed8bed3e2b3cd26c4490fcb95705fc10f
Elvis Glints <elvisglints@protonmail.com>
2019-09-11 15:56:23 +0200

    v7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Elvis Glints',                   # Type in your name
   author_email = 'elvisglints@protonmail.com',      # Type in your E-Mail
   url = 'https://github.com/glints/ePaperLibrary',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/glints/ePaperLibrary/archive/03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/glints/ePaperLibrary/archive/07.tar.gz',    # I explain this later on
   keywords = ['e-ink', 'Raspberry', 'pi'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pillow',
